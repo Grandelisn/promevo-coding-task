0c10e23a991869c6a71e087ca99caab5c2cd3cc5
Nate Grandelis <grandelisn@gmail.com>
2026-06-21 13:35:20 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+      <section className='flex justify-center items-center my-auto h-screen'>
+        <div>
+          
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
